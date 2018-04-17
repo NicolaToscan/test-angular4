5db54e70674259d1d88e230d51491ae1235d8215
Nicola Toscan <nicolatoscan99@outlook.it>
2018-04-18 00:25:24 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
   ],
   providers: [],
   bootstrap: [AppComponent]
